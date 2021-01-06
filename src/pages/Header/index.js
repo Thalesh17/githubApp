6a71a020927ce3bbe8f705e7f45e55c8cb82c7dd
Henrique Gomes Bezerra <thaalesheenrique@gmail.com>
2021-01-06 18:23:40 -0300

    ajustando imagem

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Filter from '../Filter';
 import './styles.css';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
   return (
     <div className="container">
+      <img width={51} src={logo}/>
       <div className="content">
         <Filter />
       </div>
